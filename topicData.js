aad353c06656f65c53fe54b8c396c3cddf2162ab
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2025-05-12 22:19:24 +0200

    code without react js

---
+++ topicData.js
@@ -0,0 +1,171 @@
+const topicData = {
+  analysis: {
+    title: "Analysis",
+    theory: "/data/Analysis/theorie.html",
+    exercises: [
+      {
+        title: "Aufgabe 1",
+        subexercises: [
+          {
+            question: "/data/Analysis/aufgabe1/1.html",
+            solution: "/data/Analysis/aufgabe1/L1.html"
+          },
+          {
+            question: "/data/Analysis/aufgabe1/2.html",
+            solution: "/data/Analysis/aufgabe1/L2.html"
+          },
+          {
+            question: "/data/Analysis/aufgabe1/3.html",
+            solution: "/data/Analysis/aufgabe1/L3.html"
+          }
+        ]
+      },
+      {
+        title: "Aufgabe 2",
+        subexercises: [
+          {
+            question: "/data/Analysis/aufgabe2/1.html",
+            solution: "/data/Analysis/aufgabe2/L1.html"
+          },
+          {
+            question: "/data/Analysis/aufgabe2/2.html",
+            solution: "/data/Analysis/aufgabe2/L2.html"
+          },
+          {
+            question: "/data/Analysis/aufgabe2/3.html",
+            solution: "/data/Analysis/aufgabe2/L3.html"
+          }
+        ]
+      }
+    ]
+  },
+  vektorgeometrie: {
+    title: "Vektorgeometrie",
+    theory: "/data/Vektorgeometrie/theorie.html",
+    exercises: [
+      {
+        title: "Aufgabe 1",
+        subexercises: [
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/1.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L1.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/2.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L2.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/3.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L3.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/4.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L4.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/5.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L5.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe1/6.html",
+            solution: "/data/Vektorgeometrie/aufgabe1/L6.html"
+          }
+        ]
+      },
+      {
+        title: "Aufgabe 2",
+        subexercises: [
+          {
+            question: "/data/Vektorgeometrie/aufgabe2/1.html",
+            solution: "/data/Vektorgeometrie/aufgabe2/L1.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe2/2.html",
+            solution: "/data/Vektorgeometrie/aufgabe2/L2.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe2/3.html",
+            solution: "/data/Vektorgeometrie/aufgabe2/L3.html"
+          },
+          {
+            question: "/data/Vektorgeometrie/aufgabe2/4.html",
+            solution: "/data/Vektorgeometrie/aufgabe2/L4.html"
+          }
+        ]
+      }
+    ]
+  },
+  wahrscheinlichkeit: {
+    title: "Wahrscheinlichskeitrechnung",
+    theory: "/data/Wahrscheinlichkeit/theorie.html",
+    exercises: [
+      {
+        title: "Aufgabe 1",
+        subexercises: [
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe1/1.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe1/L1.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe1/2.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe1/L2.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe1/3.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe1/L3.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe1/4.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe1/L4.html"
+          }
+        ]
+      },
+      {
+        title: "Aufgabe 2",
+        subexercises: [
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe2/1.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe2/L1.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe2/2.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe2/L2.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe2/3.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe2/L3.html"
+          }
+        ]
+      },
+      {
+        title: "Aufgabe 3",
+        subexercises: [
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/1.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L1.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/2.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L2.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/3.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L3.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/4.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L4.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/5.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L5.html"
+          },
+          {
+            question: "/data/Wahrscheinlichkeit/aufgabe3/6.html",
+            solution: "/data/Wahrscheinlichkeit/aufgabe3/L6.html"
+          }
+        ]
+      }
+    ]
+  }
+};
